0b65ef0c998c30cb47e0bf92d4137db2df0a63f7
madosuki <yurikasamaaikatsu@gmail.com>
2023-01-24 11:58:34 +0900

    fix path problem

--- src/main.rs
+++ src/main.rs
@@ -2,52 +2,84 @@
 use std::borrow::BorrowMut;
 use std::cell::RefCell;
 use std::thread;
+use std::fs::File;
+use std::io::Read;
+use std::convert::TryInto;
 
 use gio::{prelude::*, ApplicationFlags};
 use gtk::prelude::*;
 use gtk::{Application, ApplicationWindow, Button};
+use glib;
+use gdk_pixbuf;
 
 thread_local! {
     static FILE_PATH: RefCell<Option<String>> = RefCell::new(Option::None);
 }
 
+struct MainWindow {
+    main_window: ApplicationWindow,
+}
+
+impl MainWindow {
+    fn init(&self) {
+        self.main_window.set_title("First GTK+ Program");
+        self.main_window.set_default_size(1024, 768);
+        
+        let button = Button::with_label("Click me!");
+        button.connect_clicked(|_| {
+            println!("Clicked!");
+        });
+        // window.add(&button);
+        //
+
+        let _image = gtk::Image::new();
+        let mut path_string =
+            FILE_PATH.with_borrow(|x| -> String {
+                match x {
+                    Some(v) => {
+                        v.clone()
+                    },
+                    _ => "".to_owned()
+                }
+            })
+            ;
+        println!("path: {}\nbytes: {:?}", &path_string, &path_string.as_bytes());
+        // path_string.pop();
+        let path = 
+            if path_string.len() > 0 {
+                Some(std::path::Path::new(&path_string))
+            } else {
+                None
+            };
+
+        // using pixbuf loader pattern
+        let mut f = File::open(path.unwrap()).unwrap();
+        let mut buf: Vec<u8> = vec!();
+        let _ = f.read_to_end(&mut buf);
+        let pixbuf_loader = gdk_pixbuf::PixbufLoader::new();
+        pixbuf_loader.write(&buf);
+        let pixbuf_data = pixbuf_loader.pixbuf().unwrap();
+        pixbuf_loader.close();
+        _image.set_from_pixbuf(Some(&pixbuf_data));
+
+        // using set_from_file pattern
+        // _image.set_from_file(Some(path));
+
+
+        // _image.set_from_file(path);
+        let _scrolled = gtk::ScrolledWindow::builder().child(&_image).build();
+
+        self.main_window.add(&_scrolled);
+        // window.set_child(Some(&_scrolled));
+
+        self.main_window.show_all();
+        
+    }
+}
+
 fn on_activate(app: &gtk::Application) {
-    let window = ApplicationWindow::new(app);
-    window.set_title("First GTK+ Program");
-    window.set_default_size(1024, 768);
-    
-    let button = Button::with_label("Click me!");
-    button.connect_clicked(|_| {
-        println!("Clicked!");
-    });
-    // window.add(&button);
-    //
-
-    let _image = gtk::Image::new();
-    let path_string =
-        FILE_PATH.with_borrow(|x| -> String {
-            match x {
-                Some(v) => {
-                    v.clone()
-                },
-                _ => "".to_owned()
-            }
-        })
-    ;
-    println!("{}", &path_string);
-    let path = 
-        if path_string.len() > 0 {
-            Some(std::path::Path::new(&path_string))
-        } else {
-            None
-        };
-    _image.set_from_file(path);
-    let _scrolled = gtk::ScrolledWindow::builder().child(&_image).build();
-
-    window.add(&_scrolled);
-    // window.set_child(Some(&_scrolled));
-
-    window.show_all();
+    let main = MainWindow { main_window: ApplicationWindow::new(app) };
+    main.init();
 }
 
 fn local_options_handler(_app: &Application, _options: &glib::VariantDict) -> i32 {
@@ -55,9 +87,13 @@ fn local_options_handler(_app: &Application, _options: &glib::VariantDict) -> i3
 
     match _options.lookup_value("file", Option::None) {
         Some(v) => {
-            let s = v.data().iter().map(|&i| i as char).collect::<String>().trim_matches(char::from(0)).to_owned();
+            // let s = v.data().iter().map(|&i| i as char).collect::<String>().trim_matches(char::from(0)).to_owned();
+            let mut a = v.data().to_vec();
+            a.retain(|&x| x != 0);
+            let result = std::str::from_utf8(&a).unwrap().to_owned();
+
             FILE_PATH.with(|x| {
-                *x.borrow_mut() = Some(s);
+                *x.borrow_mut() = Some(result);
             })
         },
         _ => {
@@ -73,10 +109,10 @@ fn main() {
     application.add_main_option(
         "file",
         short_name, 
-  glib::OptionFlags::IN_MAIN, 
-    glib::OptionArg::Filename,
-    "descroption", 
-    Some("arg description"));
+        glib::OptionFlags::IN_MAIN, 
+        glib::OptionArg::Filename,
+        "descroption", 
+        Some("arg description"));
     
     application.connect_activate(on_activate);
     application.connect_handle_local_options(local_options_handler);
